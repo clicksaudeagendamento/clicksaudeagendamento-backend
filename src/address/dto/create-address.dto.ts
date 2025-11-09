1f7b113e9dc95d232bbfdd86342d6e130e3c973b
Leonardo Anjos <leodrums11@gmail.com>
2025-11-09 04:34:27 -0300

    remove nginx config

---
+++ src/address/dto/create-address.dto.ts
@@ -0,0 +1,35 @@
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateAddressDto {
+  @IsString()
+  @IsNotEmpty()
+  street: string;
+
+  @IsString()
+  @IsNotEmpty()
+  number: string;
+
+  @IsString()
+  @IsOptional()
+  complement?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  neighborhood: string;
+
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+
+  @IsString()
+  @IsNotEmpty()
+  state: string;
+
+  @IsString()
+  @IsNotEmpty()
+  zipCode: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}
