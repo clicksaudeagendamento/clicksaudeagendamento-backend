6442ff2c1fe4f626b8831c91e4d84e34455c7cea
Leonardo Anjos <leodrums11@gmail.com>
2025-12-02 00:06:40 -0300

    dotenv config

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,6 +25,7 @@ import { HealthModule } from './health/health.module';
       redis: {
         host: process.env.REDIS_HOST || 'localhost',
         port: +(process.env.REDIS_PORT || 6379),
+        password: process.env.REDIS_PASSWORD || undefined,
       },
     }),
     BullBoardModule.forRoot({
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as crypto from 'crypto';
